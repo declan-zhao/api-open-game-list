36f31ed1684b7c918586973187791e876dfcd3fb
Declan Zhao <zydc2010@gmail.com>
2017-09-21 16:51:23 -0400

    Add ItemsController.cs

---
+++ api-open-game-list/Controllers/ItemsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OpenGameList.ViewModels;
+using Newtonsoft.Json;
+
+namespace OpenGameList.Controllers
+{
+    [Route("api/[controller]")]
+    public class ItemsController : Controller
+    {
+        // GET api/items/GetLatest/5
+        [HttpGet("GetLatest/{num}")]
+        public JsonResult GetLatest(int num)
+        {
+            var arr = new List<ItemViewModel>();
+
+            for (int i = 1; i <= num; i++) arr.Add(
+                new ItemViewModel()
+                {
+                    Id = i,
+                    Title = String.Format("Item {0} Title", i),
+                    Description = String.Format("Item {0} Description", i)
+                });
+
+            var settings = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented
+            };
+
+            return new JsonResult(arr, settings);
+        }
+    }
+}
