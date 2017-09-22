898ae189af045a1d36b0782f89b3631b01af9367
Declan Zhao <zydc2010@gmail.com>
2017-09-21 22:05:14 -0400

    Create Item Entity

---
+++ api-open-game-list/Data/Items/Item.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OpenGameList.Data.Items
+{
+    public class Item
+    {
+        #region Constructor
+        public Item()
+        {
+        }
+        #endregion Constructor
+
+        #region Properties
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Text { get; set; }
+        public string Notes { get; set; }
+        [Required]
+        public int Type { get; set; }
+        [Required]
+        public int Flags { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public int ViewCount { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; }
+        [Required]
+        public DateTime LastModifiedDate { get; set; }
+        #endregion Properties
+    }
+}
