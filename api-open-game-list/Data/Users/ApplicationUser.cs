f8aeb26131868c8ac3e1fac814601168a036d624
Declan Zhao <zydc2010@gmail.com>
2017-09-21 22:11:07 -0400

    Create ApplicationUser Entity

---
+++ api-open-game-list/Data/Users/ApplicationUser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenGameList.Data.Users
+{
+    public class ApplicationUser
+    {
+        #region Constructor
+        public ApplicationUser()
+        {
+        }
+        #endregion Constructor
+
+        #region Properties
+        [Key]
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        [MaxLength(128)]
+        public string UserName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string DisplayName { get; set; }
+        public string Notes { get; set; }
+        [Required]
+        public int Type { get; set; }
+        [Required]
+        public int Flags { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; }
+        [Required]
+        public DateTime LastModifiedDate { get; set; }
+        #endregion Properties
+    }
+}
