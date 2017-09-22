ad5abefd4f9e63a0ced5147f4f5589b2ea64d8f9
Declan Zhao <zydc2010@gmail.com>
2017-09-22 00:38:27 -0400

    Add PostgreSQL connection config

--- api-open-game-list/Startup.cs
+++ api-open-game-list/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using OpenGameList.Data;
 
 namespace OpenGameList
 {
@@ -24,6 +26,8 @@ namespace OpenGameList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(Configuration["Data:ConnectionString:Default"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
