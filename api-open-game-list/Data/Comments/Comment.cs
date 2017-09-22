2161affdf132e7828ac0d2afaa8b9943e53565e3
Declan Zhao <zydc2010@gmail.com>
2017-09-21 22:08:07 -0400

    Create Comment Entity

---
+++ api-open-game-list/Data/Comments/Comment.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OpenGameList.Data.Comments
+{
+    public class Comment
+    {
+        #region Constructor
+        public Comment()
+        {
+        }
+        #endregion Constructor
+
+        #region Properties
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public int ItemId { get; set; }
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        public int Type { get; set; }
+        [Required]
+        public int Flags { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        public int? ParentId { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; }
+        [Required]
+        public DateTime LastModifiedDate { get; set; }
+        #endregion Properties
+    }
+}
